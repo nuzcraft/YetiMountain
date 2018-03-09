23348064bf07ee8804d7ad248a57f56b7feb1709
nuzcraft <nuzcraft@gmail.com>
2018-03-09 07:01:55 -0600

    Minor config and variable changes
    I literally added a single unused variable and changed the window size
    lol

--- variables.py
+++ variables.py
@@ -1,5 +1,4 @@
 # global variables to be used
-
 player_array = [(10, 10), (10, 11)]
 glyph_array = ['@', '/']
 
@@ -10,6 +9,9 @@ entities = []
 
 player_action = 'idle'
 game_state = 'playing'
+
+# create a turn number at the start
+turn_number = 0
 
 # mouse state
 mouse_x = 0
